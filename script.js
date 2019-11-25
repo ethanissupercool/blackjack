8f6ad33fa0e596698e100e96fe731f3757ecbccc
ethan <wizardcat@pm.me>
2019-11-26 09:45:29 +1100

    commit
    got shit done added cpu

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+//blackjack ethan huang y12 assessment 1
+
 nonactiveDeck = ['AC', 'AD', 'AH', 'AS', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9S', '9C', '9D', '9H', 'TS', 'TC', 'TD', 'TH', 'JS', 'JC', 'JD', 'JH', 'QS', 'QC', 'QD', 'QH', 'KS', 'KC', 'KD', 'KH']
 deck = ['AC', 'AD', 'AH', 'AS', '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9S', '9C', '9D', '9H', 'TS', 'TC', 'TD', 'TH', 'JS', 'JC', 'JD', 'JH', 'QS', 'QC', 'QD', 'QH', 'KS', 'KC', 'KD', 'KH']
 dealerHandValue = 0;
 playerHandValue = 0;
+cpuHandValue = 0;
 
 $(document).ready(function(){
+  if (localStorage.getItem("wallet") == null){
+    localStorage.setItem("wallet", 100);
+    $('#dealerValue').text("Wallet: " + localStorage.getItem("wallet"));
+  }
+  $('#dealerValue').text('Wallet: ' + localStorage.getItem("wallet"));
   console.log(deck)
   console.log("shuffling")
   shuffle(1000, deck)
   console.log("images provided courtesy of american contact bridge league")
+
+//dealer initial draw
   $('#dealerHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
   var cardValue = deck[0][0];
   console.log('called')
@@ -38,6 +48,8 @@ $(document).ready(function(){
     deck.shift();
   }
 
+
+//player initial draw
   $('#playerHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
   var cardValue = deck[0][0];
   if (cardValue == 'J' || cardValue == 'Q' || cardValue == 'K' || cardValue == 'T'){
@@ -66,10 +78,43 @@ $(document).ready(function(){
     $('#playerValue').text('Player Value: ' + playerHandValue)
     deck.shift();
   }
+
+
+//CPU initial draw
+  $('#cpuHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
+  var cardValue = deck[0][0];
+  if (cardValue == 'J' || cardValue == 'Q' || cardValue == 'K' || cardValue == 'T'){
+    cardValue = 10;
+    cpuHandValue = cpuHandValue + parseInt(cardValue);
+    $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+    deck.shift();
+  }
+  if (cardValue == 'A'){ //ace
+    console.log("ACE DRAWN")
+    cardValue = 11;
+    if (cpuHandValue + parseInt(cardValue) > 21){
+      cardValue = 1;
+      cpuHandValue = cpuHandValue + parseInt(cardValue);
+      $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+      deck.shift();
+    }
+    else if (cpuHandValue + parseInt(cardValue) <= 21){
+      cpuHandValue = cpuHandValue + parseInt(cardValue);
+      $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+      deck.shift();
+    }
+  }
+  if (cardValue == '2' || cardValue == '3' || cardValue == '4' || cardValue == '5' || cardValue == '6' || cardValue == '7' || cardValue == '8' || cardValue == '9') {
+    cpuHandValue = cpuHandValue + parseInt(cardValue);
+    $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+    deck.shift();
+  }
 })
 
 
 function stand() {
+
+  //DEALER DRAWS
   while (dealerHandValue < 17){
     $('#dealerHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
     var cardValue = deck[0][0];
@@ -102,31 +147,92 @@ function stand() {
       deck.shift();
     }
   }
+
+  //CPU DRAWS
+  while (cpuHandValue < 17){
+    $('#cpuHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
+    var cardValue = deck[0][0];
+    if (cardValue == 'J' || cardValue == 'Q' || cardValue == 'K' || cardValue == 'T'){
+      cardValue = 10;
+      console.log("b"+cpuHandValue);
+      cpuHandValue = cpuHandValue + parseInt(cardValue);
+      console.log("a"+cpuHandValue);
+      $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+      deck.shift();
+    }
+    if (cardValue == 'A'){ //ace
+      console.log("hi")
+      cardValue = 11;
+      if (cpuHandValue + parseInt(cardValue) > 21){
+        cardValue = 1;
+        cpuHandValue = cpuHandValue + parseInt(cardValue);
+        $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+        deck.shift();
+      }
+      else if (cpuHandValue + parseInt(cardValue) <= 21){
+        cpuHandValue = cpuHandValue + parseInt(cardValue);
+        $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+        deck.shift();
+      }
+    }
+    if (cardValue == '2' || cardValue == '3' || cardValue == '4' || cardValue == '5' || cardValue == '6' || cardValue == '7' || cardValue == '8' || cardValue == '9') {
+      cpuHandValue = cpuHandValue + parseInt(cardValue);
+      $('#cpuValue').text('cpu Value: ' + cpuHandValue)
+      deck.shift();
+    }
+  }
+
   if (dealerHandValue == playerHandValue){
-    console.log('tie');
+    alert('tie');
+    dealerValue = 0;
+    playerValue = 0;
+    $("#dealerHand th").remove();
+    $("#playerHand th").remove();
   }
 
   if (dealerHandValue > 21 && playerHandValue <= 21){
-    console.log('player win');
+    alert('player win');
+    dealerValue = 0;
+    playerValue = 0;
+    $("#dealerHand th").remove();
+    $("#playerHand th").remove();
   }
 
   if (playerHandValue > 21 && dealerHandValue <= 21){
-    console.log('dealer win');
+    alert('dealer win');
+    dealerValue = 0;
+    playerValue = 0;
+    $("#dealerHand th").remove();
+    $("#playerHand th").remove();
   }
 
   if (playerHandValue == 21){
-    console.log('player win');
+    alert('player win');
+    dealerValue = 0;
+    playerValue = 0;
   }
   if (dealerHandValue == 21){
-    console.log('dealer win');
+    alert('dealer win');
+    dealerValue = 0;
+    playerValue = 0;
+    $("#dealerHand th").remove();
+    $("#playerHand th").remove();
   }
 
   if (playerHandValue < 21 && dealerHandValue < 21){
     if (dealerHandValue < playerHandValue){
-      console.log('player win');
+      alert('player win');
+      dealerValue = 0;
+      playerValue = 0;
+      $("#dealerHand th").remove();
+      $("#playerHand th").remove();
     }
     if (playerHandValue < dealerHandValue){
-      console.log('dealer win');
+      alert('dealer win');
+      dealerValue = 0;
+      playerValue = 0;
+      $("#dealerHand th").remove();
+      $("#playerHand th").remove();
     }
   }
 }
@@ -171,6 +277,7 @@ function hit(){
   //player hit
   $('#playerHand').append('<th><img src="assets/'+deck[0]+'.png" id="image"></th>');
   var cardValue = deck[0][0];
+  //checkBust(cardValue, playerHandValue);
   if (cardValue == 'J' || cardValue == 'Q' || cardValue == 'K' || cardValue == 'T'){
     cardValue = 10;
     playerHandValue = playerHandValue + parseInt(cardValue);
@@ -183,12 +290,12 @@ function hit(){
       if (playerHandValue + parseInt(cardValue) > 21){
         cardValue = 1;
         playerHandValue = playerHandValue + parseInt(cardValue);
-        $('#playerValue').text('player Value: ' + playerHandValue)
+        $('#playerValue').text('Player Value: ' + playerHandValue)
         deck.shift();
       }
       else if (playerHandValue + parseInt(cardValue) <= 21){
         playerHandValue = playerHandValue + parseInt(cardValue);
-        $('#playerValue').text('player Value: ' + playerHandValue)
+        $('#playerValue').text('Player Value: ' + playerHandValue)
         deck.shift();
       }
     }
@@ -205,9 +312,14 @@ function shuffle(shuffleAmt, a) {
   }
 }
 
-function checkBust(){
-
-
+function checkBust(a, b){
+  console.log(parseInt(a) + parseInt(b));
+  if (parseInt(a) + parseInt(b) > 21){
+    alert("bust");
+    $("#dealerHand th").remove();
+    $("#playerHand th").remove();
+    $("#cpuHand th").remove();
+  }
 }
 
 function shuffleCards(a) {
